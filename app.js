b81e235317f70712db49e4841dd32bfca1db0e47
francis omondi <francisomondi17@gmail.com>
2022-08-22 17:28:26 +0300

    loading our home/index page

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express")
 const app = express()
-const port = process.env.port || 4000;
+const port = process.env.port || 3000;
+const bodyparser = require("body-parser")
+
+app.set("view engine", "ejs")
+app.use(bodyparser.urlencoded({
+    extended: false
+}))
+app.use(bodyparser.json())
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
 
 app.listen(port, () => {
     console.log(`Listning to port ${port}`)
